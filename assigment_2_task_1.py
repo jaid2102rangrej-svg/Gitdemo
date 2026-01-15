7a64e9f905d579342b5c9f1bc0817dc69294301a
jaid2102rangrej-svg <jaid2102rangrej@gmail.com>
2026-01-15 16:30:51 +0100

    Add files via upload

---
+++ assigment_2_task_1.py
@@ -0,0 +1,6 @@
+# Task 1: Check if a Number is Even or Odd
+num = int(input("enter a number: "))
+if num % 2 == 0:
+    print(num,"is an even number.")
+else:
+    print(num,"is an odd number.")
---
+++ assigment_2_task_2.py
@@ -0,0 +1,10 @@
+
+total = 0
+
+for num in range(1, 51):
+    total += num
+
+
+print(f"The sum of numbers from 1 to 50 is: {total}")
+
+
